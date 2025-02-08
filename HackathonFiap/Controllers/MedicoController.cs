14b3273b9aad84d8c0dc398129d4dcda050a5227
cleitonzummach <cleitonzummach@gmail.com>
2025-02-08 20:01:29 -0300

    Update MedicoController.cs

--- HackathonFiap/Controllers/MedicoController.cs
+++ HackathonFiap/Controllers/MedicoController.cs
@@ -23,5 +23,28 @@ namespace HackathonFiap.Controllers
 
             return Ok(medicos);
         }
+
+        [HttpPost("CadastrarHorario")]
+        public async Task<IActionResult> CadastrarHorario(DateTime data)
+        {
+            //http://localhost:5131/api/HorarioDisponivel
+
+            var values = new Dictionary<string, string>
+            {
+                { "MedicoId", "1" },
+                { "Data", data.Date.ToString("dd/MM/yyyy") },
+                { "Hora", data.Date.ToString("HH:mm") },
+                { "Disponivel", "true" }
+            };
+
+            var content = new FormUrlEncodedContent(values);
+
+            HttpClient client = new HttpClient();
+            var response = await client.PostAsync("http://localhost:5131/api/HorarioDisponivel", content);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            return Ok();
+        }
     }
 }
